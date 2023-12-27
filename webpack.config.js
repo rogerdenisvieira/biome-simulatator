961ce6fa4d9fbf6b8821745396b324171a41337b
Roger Vieira <rogerdenisvieira@gmail.com>
2023-12-27 20:38:12 -0300

    added sample code

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/game.ts",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "./assets" }],
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        include: path.resolve(__dirname, "src"),
+        loader: "ts-loader",
+      },
+      {
+        test: require.resolve("phaser"),
+        loader: "expose-loader",
+        options: { exposes: { globalName: "Phaser", override: true } },
+      },
+    ],
+  },
+  devServer: {
+    static: path.resolve(__dirname, "./"),
+    host: "localhost",
+    port: 8080,
+    open: false,
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+};
